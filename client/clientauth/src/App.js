1287cbfedc0ca24bc9c5174dd142cd0b6e24c75d
bri79319 <bri79319@gmail.com>
2018-09-21 16:41:10 -0400

    app.js set up

--- client/clientauth/src/App.js
+++ client/clientauth/src/App.js
@@ -14,12 +14,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route path="/jokes" component={Jokes} />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
