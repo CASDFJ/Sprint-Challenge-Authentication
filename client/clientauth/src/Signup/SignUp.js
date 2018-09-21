9776a5045ca63190d39b07b07d97e4e1c286a6c0
bri79319 <bri79319@gmail.com>
2018-09-21 18:14:02 -0400

    still debugging port

--- client/clientauth/src/Signup/SignUp.js
+++ client/clientauth/src/Signup/SignUp.js
@@ -45,7 +45,7 @@ class SignUp extends Component {
     console.log(this.state);
 
     axios
-      .post("http://localhost:1000/api/register", this.state)
+      .post("http://localhost:6500/api/register", this.state)
       .then(res => {
         console.log(res.data);
         // localStorage.setItem("jwt", res.data.token);
