d040a7142d6870f7cf09948297be8939698e35f5
bri79319 <bri79319@gmail.com>
2018-09-21 16:47:55 -0400

    edited some code

--- client/clientauth/src/App.js
+++ client/clientauth/src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { Route, withRouter } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
-import Register from "./register/Register.js";
-import Login from "./auth/Login.js";
-import Jokes from "./jokes/Jokes.js"
+import SignUp from "./Signup/SignUp.js";
+import SignIn from "./Signin/SignIn.js";
+import Jokes from "./Jokes/Jokes.js"
 
 class App extends Component {
   render() {
@@ -14,9 +14,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Route path="/register" component={Register} />
-        <Route path="/login" component={Login} />
-        <Route path="/jokes" component={Jokes} />
+        <Route path="/SignUp" component={SignUp} />
+        <Route path="/SignIn" component={SignIn} />
+        <Route path="/Jokes" component={Jokes} />
       </div>
     );
   }
